c6bb6ad1b169faab97fef3deae329aa8238e1912
prathiusha22 <116252266+prathiusha22@users.noreply.github.com>
2023-06-23 10:26:35 +0530

    day17-mvm-project-commits

---
+++ src/main/java/day17/solved/HelloWorldProg.java
@@ -0,0 +1,10 @@
+package day17.solved;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello World!");
+	}
+
+}
