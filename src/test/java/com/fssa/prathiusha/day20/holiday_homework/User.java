5bdf447ab04362c02caac41b9c6fa51a414270e3
prathiusha22 <prathiusha22@gmail.com>
2023-07-02 20:59:46 +0530

    Holiday Homework commits

---
+++ src/test/java/com/fssa/prathiusha/day20/holiday_homework/User.java
@@ -0,0 +1,34 @@
+package com.fssa.prathiusha.day20.holiday_homework;
+
+public class User {
+	String firstname;
+	String lastname;
+	String emailId;
+	String password;
+	public String getFirstName() {
+		return firstname;
+	}
+	public void setName(String name) {
+		this.firstname = name;
+	}
+	
+	public String getLastName() {
+		return lastname;
+	}
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
